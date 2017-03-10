b61fbbe65b2daae4cf3f76b95a33d8b7a3f136fb
unknown <estudiante@LAB0240.SERVERINT.LOCAL>
2017-03-10 12:08:42 -0600

    hola mundo

---
+++ funciones.js
@@ -0,0 +1,7 @@
+var	BacoB=angular.module("BacoB",[]);
+BcoB.controller("mostrar",function ($scope){
+	$scope.Mensaje = "Evelyn"
+	$scope.operaciones = $Scope.first*$Scope.second;
+	$Scope.first=2
+	$Scope.second=5
+});
